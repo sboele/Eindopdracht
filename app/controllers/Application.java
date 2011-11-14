09c5f214bfde91fdc1b96e29051589ae269f25d4
Leonard Punt <leonardpunt@gmail.com>
2011-11-14 17:21:43 +0100

    Some refactoring

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,8 +8,8 @@ import java.util.List;
 
 public class Application extends Controller {
 
-    public static void index() {
-        List<List<String>> rows = new Helper().getBodyLotionData();
-        render(rows);
-    }
+	public static void index() {
+		List<List<String>> rows = new Helper().getBodyLotionData();
+		render(rows);
+	}
 }
--- app/helpers/Helper.java
+++ app/helpers/Helper.java
@@ -8,34 +8,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- *
+ * 
  * @author Sander
  */
 public class Helper {
-    public List<List<String>> getBodyLotionData()
-    {
-        List<List<String>> rows = new ArrayList<List<String>>();
-        
-        String[][] data = new String[][]{
-            {"Sarah", "blond", "1,63", "49", "nee", "merkbaar"},
-            {"Alex", "blond", "1,85", "70", "ja", "gering"},
-            {"Diana", "bruin", "1,50", "51", "ja", "gering"},
-            {"Anne", "blond", "1,55", "54", "nee", "merkbaar"},
-            {"Emily", "rood", "1,67", "80", "nee", "merkbaar"},
-            {"Peter", "bruin", "1,81", "90", "nee", "gering"},
-            {"Jan", "bruin", "1,61", "76", "nee", "gering"},
-            {"Katie", "bruin", "1,53", "43", "ja", "gering"}
-        };
-        
-        for(int i = 0 ; i < data.length ; i++)
-        {
-            List<String> temp = new ArrayList<String>();
-            for(int j = 0 ; j < data[i].length ; j++)
-            {
-                temp.add(data[i][j]);
-            }
-            rows.add(temp);
-        }
-        return rows;
-    }
+
+	String[][] bodyLotionData = new String[][] {
+			{ "Sarah", "blond", "1,63", "49", "nee", "merkbaar" },
+			{ "Alex", "blond", "1,85", "70", "ja", "gering" },
+			{ "Diana", "bruin", "1,50", "51", "ja", "gering" },
+			{ "Anne", "blond", "1,55", "54", "nee", "merkbaar" },
+			{ "Emily", "rood", "1,67", "80", "nee", "merkbaar" },
+			{ "Peter", "bruin", "1,81", "90", "nee", "gering" },
+			{ "Jan", "bruin", "1,61", "76", "nee", "gering" },
+			{ "Katie", "bruin", "1,53", "43", "ja", "gering" } };
+
+	public List<List<String>> getBodyLotionData() {
+		List<List<String>> rows = new ArrayList<List<String>>();
+
+		for (int i = 0; i < bodyLotionData.length; i++) {
+			List<String> temp = new ArrayList<String>();
+			for (int j = 0; j < bodyLotionData[i].length; j++) {
+				temp.add(bodyLotionData[i][j]);
+			}
+			rows.add(temp);
+		}
+		
+		return rows;
+	}
 }
