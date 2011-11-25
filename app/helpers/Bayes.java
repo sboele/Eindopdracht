3f9908652d3a35b8ed4e043ae2d7814d190e77f5
Sander Boele <boeluhh@gmail.com>
2011-11-25 15:49:47 +0100

    Naive bayes table modified Naive bayes for numeric values added

--- app/helpers/Bayes.java
+++ app/helpers/Bayes.java
@@ -5,7 +5,6 @@
 package helpers;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -13,33 +12,68 @@ import java.util.Map;
  *
  * @author Sander
  */
+// TODO: Total gering/merkbaar in one row
 public class Bayes {
 
     Data data = new helpers.Data();
     private int numberOfColumns = 6;
-    private int maxValues = data.maxNumberOfUniqueValuesOrdinal();
+    private int maxValuesOrdinal = data.maxNumberOfUniqueValuesOrdinal();
+    private int maxValuesNumeric = data.maxNumberOfUniqueValuesNumeric();
     List<List<String>> rows = new ArrayList<List<String>>();
 
-    public List<List<String>> getBayesData() {
+    public List<List<String>> getNaiveBayesNominal() {
         rows.add(getPossibleOutcomesWithEmptyColumn());
-        createBayesTable(false);
-        createBayesTable(true);
+        createBayesTableNominal(false);
+        createBayesTableNominal(true);
         return rows;
     }
 
-    public void createBayesTable(boolean summary) {
-        for (int i = 0; i < maxValues; i++) {
+    public List<List<String>> getNaiveBayesNumeric() {
+        rows.add(getPossibleOutcomesWithEmptyColumn());
+        createBayesTableNumeric(false);
+        return rows;
+    }
+
+    public void createBayesTableNominal(boolean summary) {
+        for (int i = 0; i < maxValuesOrdinal; i++) {
             List<String> l = new ArrayList<String>();
             for (int j = 1; j < numberOfColumns; j++) {
                 if (data.getUniqueValuesForAttributeInOrdinalData(j, false).size() > i) {
                     l.add(data.getUniqueValuesForAttributeInOrdinalData(j, false).get(i));
                     if (summary) {
-                        l.add("" + data.getNumberOfMerkbaar(data.getUniqueValuesForAttributeInOrdinalData(j, false).get(i)) + "/" + data.getGroupedValuesForAttributeInOrdinalData(numberOfColumns - 1, false).get("merkbaar"));
-                        l.add("" + data.getNumberOfGering(data.getUniqueValuesForAttributeInOrdinalData(j, false).get(i)) + "/" + data.getGroupedValuesForAttributeInOrdinalData(numberOfColumns - 1, false).get("gering"));
+                        if (j == numberOfColumns - 1) {
+                            l.add("" + data.getNumberOfMerkbaar(data.getUniqueValuesForAttributeInOrdinalData(j, false).get(i)) + "/" + (data.getOrdinalBodyLotionData().size() - 1));
+                            l.add("" + data.getNumberOfGering(data.getUniqueValuesForAttributeInOrdinalData(j, false).get(i)) + "/" + (data.getOrdinalBodyLotionData().size() - 1));
+                        } else {
+                            l.add("" + data.getNumberOfMerkbaar(data.getUniqueValuesForAttributeInOrdinalData(j, false).get(i)) + "/" + data.getGroupedValuesForAttributeInOrdinalData(numberOfColumns - 1, false).get("merkbaar"));
+                            l.add("" + data.getNumberOfGering(data.getUniqueValuesForAttributeInOrdinalData(j, false).get(i)) + "/" + data.getGroupedValuesForAttributeInOrdinalData(numberOfColumns - 1, false).get("gering"));
+                        }
                     } else {
                         l.add("" + data.getNumberOfMerkbaar(data.getUniqueValuesForAttributeInOrdinalData(j, false).get(i)));
                         l.add("" + data.getNumberOfGering(data.getUniqueValuesForAttributeInOrdinalData(j, false).get(i)));
                     }
+                } else {
+                    l.add("");
+                    l.add("");
+                    l.add("");
+                }
+            }
+            rows.add(l);
+        }
+    }
+
+    public void createBayesTableNumeric(boolean summary) {
+        for (int i = 0; i < maxValuesNumeric; i++) {
+            List<String> l = new ArrayList<String>();
+            for (int j = 1; j < numberOfColumns; j++) {
+                if (data.getUniqueValuesForAttributeInNumericData(j, false).size() > i) {
+                    l.add(data.getUniqueValuesForAttributeInNumericData(j, false).get(i));
+                    l.add("" + data.getNumberOfMerkbaar(data.getUniqueValuesForAttributeInNumericData(j, false).get(i)));
+                    l.add("" + data.getNumberOfGering(data.getUniqueValuesForAttributeInNumericData(j, false).get(i)));
+                } else {
+                    l.add("");
+                    l.add("");
+                    l.add("");
                 }
             }
             rows.add(l);
@@ -50,10 +84,9 @@ public class Bayes {
         List<String> row = new ArrayList<String>();
         for (int i = 0; i < numberOfColumns - 1; i++) {
             row.add("");
-            Iterator it = data.getGroupedValuesForAttributeInOrdinalData(numberOfColumns - 1, false).entrySet().iterator();
-            while (it.hasNext()) {
-                Map.Entry pairs = (Map.Entry) it.next();
-                row.add(pairs.getKey().toString());
+            Map<String, Integer> map = data.getGroupedValuesForAttributeInOrdinalData(numberOfColumns - 1, false);
+            for (String key : map.keySet()) {
+                row.add(key);
             }
         }
         return row;
