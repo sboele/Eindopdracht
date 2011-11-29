82975e50db17d8e0e5ff0d883add04c26eb152d5
Leonard Punt <leonardpunt@gmail.com>
2011-11-29 17:04:18 +0100

    Some refactoring

--- app/helpers/Statistics.java
+++ app/helpers/Statistics.java
@@ -4,26 +4,35 @@ import java.util.List;
 
 public class Statistics {
 	
-	public double probabilityDensity(double x, double mean, double standardDeviation) {
+	public double getProbabilityDensity(double x, double mean, double standardDeviation) {
         double macht = -1 * (Math.pow(x-mean, 2) / (2 * Math.pow(standardDeviation, 2)));
         double fx = 1.0 / (Math.sqrt(2 * Math.PI) * standardDeviation) * Math.pow(Math.E,macht);
         return fx;
 	}
 	
-	public double mean(List<Double> numbers) {
+	public double getMean(List<Double> numbers) {
 		double total = 0.0;
 		for (double number : numbers)
 			total += number;
 		return total/numbers.size();
 	}
 	
-	public double standardDeviation(List<Double> numbers) {
-		double mean = mean(numbers);
+	public double getStandardDeviation(List<Double> numbers) {
+		double mean = getMean(numbers);
 		double numerator = 0.0;
 		double denominator = numbers.size() - 1;
 		for (double number: numbers)
 			numerator += Math.pow(number - mean, 2);
 		return Math.sqrt(numerator/denominator);
+	}
+	
+	public double getProbability(double likeliHoodMerkbaar, double likeliHoodGering, boolean forMerkbaar) {
+		double probability = 0.0;
+		if (forMerkbaar)
+			probability = likeliHoodMerkbaar / (likeliHoodMerkbaar + likeliHoodGering);
+		else
+			probability = likeliHoodGering / (likeliHoodMerkbaar + likeliHoodGering);
+		return probability*100;
 	}
 
 }
