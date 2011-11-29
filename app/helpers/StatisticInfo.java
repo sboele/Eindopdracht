083baa14175462688938825829234fc45189a108
Sander Boele <boeluhh@gmail.com>
2011-11-29 19:47:51 +0100

    Added class for statisticinfo

---
+++ app/helpers/StatisticInfo.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helpers;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author Sander
+ */
+public class StatisticInfo {
+    Data data;
+    
+    public StatisticInfo() {
+        data = new Data();
+    }
+    
+    public Map<String, List<String>> getStatisticInfo()
+    {
+        Map<String, List<String>> result = new HashMap<String, List<String>>();
+        
+        return result;
+    }
+}
--- app/helpers/Statistics.java
+++ app/helpers/Statistics.java
@@ -5,9 +5,9 @@ import java.util.List;
 public class Statistics {
 	
 	public double probabilityDensity(double x, double mean, double standardDeviation) {
-        double macht = -1 * (Math.pow(x-mean, 2) / (2 * Math.pow(standardDeviation, 2)));
-        double fx = 1.0 / (Math.sqrt(2 * Math.PI) * standardDeviation) * Math.pow(Math.E,macht);
-        return fx;
+            double macht = -1 * (Math.pow(x-mean, 2) / (2 * Math.pow(standardDeviation, 2)));
+            double fx = 1.0 / (Math.sqrt(2 * Math.PI) * standardDeviation) * Math.pow(Math.E,macht);
+            return fx;
 	}
 	
 	public double mean(List<Double> numbers) {
